d005303654f45384c5aff3f270c00cf25c99202e
Roxanne Panthaky <rpanthak@ciena.com>
2017-08-05 23:13:03 -0400

    lint fixes

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,13 @@
 import Ember from 'ember'
+const {Router} = Ember
 import config from './config/environment'
 
-var Router = Ember.Router.extend({
+var AppRouter = Router.extend({
   location: config.locationType
 })
 
-Router.map(function () {
-  this.route('demo', { path: '/' })
+AppRouter.map(function () {
+  this.route('demo', {path: '/'})
 })
 
-export default Router
+export default AppRouter
